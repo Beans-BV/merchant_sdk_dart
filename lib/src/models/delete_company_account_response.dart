5e96ba17ac180daab5be17293761b4c704d44650
Jop Middelkamp <middelkamp@live.nl>
2025-10-28 22:25:25 +0100

    fix: DeleteCompanyAccountResponse

--- lib/src/models/delete_company_account_response.dart
+++ lib/src/models/delete_company_account_response.dart
@@ -1,27 +1,10 @@
-import 'company_account.dart';
-
 /// Response object returned when deleting a company account
 class DeleteCompanyAccountResponse {
-  /// The deleted account
-  final CompanyAccount account;
-
-  /// The status of the deletion operation
-  final String status;
-
-  DeleteCompanyAccountResponse({
-    required this.account,
-    required this.status,
-  });
+  DeleteCompanyAccountResponse();
 
   factory DeleteCompanyAccountResponse.fromJson(Map<String, dynamic> json) {
-    return DeleteCompanyAccountResponse(
-      account: CompanyAccount.fromJson(json['account'] as Map<String, dynamic>),
-      status: json['status'] as String,
-    );
+    return DeleteCompanyAccountResponse();
   }
 
-  Map<String, dynamic> toJson() => {
-        'account': account.toJson(),
-        'status': status,
-      };
+  Map<String, dynamic> toJson() => {};
 }
